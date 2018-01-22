1ae515f707039c8d79ba8a4714dc7b181c543cd6
Brad Efting <bradefting@Brad-Eftings-MacBook-Pro-11.local>
2018-01-22 08:29:48 -0700

    Add GTM code

--- public/js/app/app.module.js
+++ public/js/app/app.module.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('app', ['ui.router', 'angularMoment', 'vcRecaptcha']);
+  angular.module('app', ['ui.router', 'angularMoment', 'vcRecaptcha', 'angulartics']);
   // angular.module('app', ['ui.router', 'angularMoment', 'angulartics', 'angulartics.google.tagmanager']);
 
 }());
